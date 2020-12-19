11890e9b0c2c108d2317850e9c5eaaccf79a00e3
Keith <mutambirwa.keith@gmail.com>
2020-12-19 23:43:19 +0200

    refactor/update app to use package structure

---
+++ apc/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ['SQLALCHEMY_TRACK_MODIFICATIONS']
+db = SQLAlchemy(app)
+
+from apc import routes
